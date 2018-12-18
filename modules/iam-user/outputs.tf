964a86f37c04154285af4bbeb462f8dda1566794
arulkumarkandasamy <karul43@yahoo.co.in>
2018-12-18 23:43:22 +0000

    Fomatting output

--- modules/iam-user/outputs.tf
+++ modules/iam-user/outputs.tf
@@ -1,4 +1,4 @@
 output "test_iam_user_name_with_access_key" {
-  value = "${join(",", concat(aws_iam_user.test.*.name,aws_iam_access_key.test.*.id,aws_iam_access_key.test.*.secret))}"
+  value = "${formatlist("%v - %v - %v", aws_iam_user.test.*.name, aws_iam_access_key.test.*.id,aws_iam_access_key.test.*.secret)}"
 }
 
